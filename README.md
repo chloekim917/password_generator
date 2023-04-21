f7459af9db8b3ee347bbcc17132b7e7c1ac756b1
chloekim917 <chloekim917@gmail.com>
2023-04-21 13:01:58 +0900

    readme and pw length selectioin added

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a password generator.
+The system asks the user how many characters the user want the password to be. 
+When user gives an answer, the system randomly chooses chooses characters from alphabet(capital letters included), numbers, and special characters, compiles them, and displays the password in the length that the user requested. 
--- app.py
+++ app.py
@@ -4,10 +4,11 @@ import string
 letters = string.ascii_letters #alphabet including capitalized letters
 digits = string.digits #0 to 9
 special_characters = "!@#$%^&*()_+-=[]{}|;:\"'<>,.?/" #backward slash let's double quotation marks be a string
+length = int(input("How many characters do you want the password to have? "))
 
-random_string = ''.join(random.choice(letters + digits + special_characters) for _ in range(12))
+random_string = ''.join(random.choice(letters + digits + special_characters) for _ in range(length))
 #''.join -- compiles the result into a string
-#for _ in range(12) -- generates a sequence of 12 random characters by calling the choice() function inside a for loop. The loop runs 12 times.
+#for _ in range(12) -- generates a sequence of 'length' number of random characters by calling the choice() function inside a for loop. The loop runs 'length' many times.
 
 
 print(random_string)
