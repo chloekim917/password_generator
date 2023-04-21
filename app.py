49bdcebdcceed8b48117138c9c704d8503674876
chloekim917 <chloekim917@gmail.com>
2023-04-21 12:52:46 +0900

    comments for my reference

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import random
 import string
 
-letters = string.ascii_letters
-digits = string.digits
-special_characters = "!@#$%^&*()_+-=[]{}|;:\"'<>,.?/"
+letters = string.ascii_letters #alphabet including capitalized letters
+digits = string.digits #0 to 9
+special_characters = "!@#$%^&*()_+-=[]{}|;:\"'<>,.?/" #backward slash let's double quotation marks be a string
 
 random_string = ''.join(random.choice(letters + digits + special_characters) for _ in range(12))
+#''.join -- compiles the result into a string
+#for _ in range(12) -- generates a sequence of 12 random characters by calling the choice() function inside a for loop. The loop runs 12 times.
+
+
 print(random_string)
